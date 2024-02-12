e79ea9b9af4236f4d8fcda6b2c596be360dfa633
Sahil Mondal <86883241+sahilmondal@users.noreply.github.com>
2024-02-12 22:00:29 +0530

    Initial Upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { MotionDiv } from "@/components/MotionDiv";
+export default function Home() {
+  return (
+    <main className="">
+      <MotionDiv>Sahil Mondal</MotionDiv>
+    </main>
+  );
+}
